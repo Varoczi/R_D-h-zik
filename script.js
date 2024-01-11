cae510a8d57c98d5ff8a17adc51ccbfcc4f49f26
Varoczi <varoczigergo@gmail.com>
2024-01-11 21:09:14 +0100

    start

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function toggleLightMode() {
     document.body.classList.toggle('light-mode');
     
-    var articles = document.querySelectorAll('article');
-    var legends = document.querySelectorAll('legend');
-    var labels = document.querySelectorAll('label');
+    let articles = document.querySelectorAll('article');
+    let legends = document.querySelectorAll('legend');
+    let labels = document.querySelectorAll('label');
     articles.forEach(function(article) {
         article.classList.toggle('light-mode');
     });
