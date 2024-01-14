45f8f562edf1aed158ec1d72a5dbf00a7ea44cc9
Varoczi <varoczigergo@gmail.com>
2024-01-14 18:27:40 +0100

    6. hazi

--- script.js
+++ script.js
@@ -31,3 +31,45 @@ function percSzamitas () {
 }
 
 percSzamitas();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.querySelector('.search');
+    const article = document.getElementById('article');
+    const originalContent = article.innerHTML;
+
+    searchInput.addEventListener('input', function (e) {
+        const value = e.target.value.trim();
+
+        if (value === "") {
+            article.innerHTML = originalContent;
+            return;
+        }
+
+        highlightText(article, originalContent, value);
+    });
+});
+
+function highlightText(element, content, searchText) {
+    const escapedSearchText = searchText.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    const regex = new RegExp(`(${escapedSearchText})`, 'gi');
+
+    function processNodes(node) {
+        if (node.nodeType === 3) {
+            const text = node.nodeValue;
+            const spannedText = text.replace(regex, '<span class="search-highlight">$1</span>');
+            const tempElement = document.createElement('div');
+            tempElement.innerHTML = spannedText;
+            while (tempElement.firstChild) {
+                node.parentNode.insertBefore(tempElement.firstChild, node);
+            }
+            node.parentNode.removeChild(node);
+        } else if (node.nodeType === 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
+            for (let i = 0; i < node.childNodes.length; i++) {
+                processNodes(node.childNodes[i]);
+            }
+        }
+    }
+
+    element.innerHTML = content;
+    processNodes(element);
+}
